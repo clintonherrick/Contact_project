322bf5b6f41ac7e1b61d56c35cfee0ccc9494e76
Janice Dunn & Clinton Herrick <student@epicodus.com>
2015-08-13 12:27:38 -0700

    Save a new mailing address to the mailing address list.

--- lib/mailing_address.rb
+++ lib/mailing_address.rb
@@ -1,5 +1,6 @@
 class Mailing_Address
 
+  @@all_mailing_addresses = []
 
   define_method(:initialize) do |street_address, city, state, zip_code|
     @street_address = street_address
@@ -22,6 +23,18 @@ class Mailing_Address
 
   define_method(:zip_code) do
     @zip_code
+  end
+
+  define_singleton_method(:all) do
+    @@all_mailing_addresses
+  end
+
+  define_method(:save) do
+    @@all_mailing_addresses.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_mailing_addresses = []
   end
 
 end
--- spec/mailing_address_spec.rb
+++ spec/mailing_address_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('mailing_address')
 
 describe(Mailing_Address) do
+  before() do
+    Mailing_Address.clear()
+  end
 
   describe('#street_address') do
     it("returns the street address of the mailing address") do
@@ -30,5 +33,29 @@ describe(Mailing_Address) do
       expect(test_mailing_address.zip_code()).to(eq(97201))
     end
   end
+
+  describe('.all') do
+    it("is empty at first") do
+      expect(Mailing_Address.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a mailing address to the array of saved mailing address") do
+      test_mailing_address = Mailing_Address.new("123 Real Street", "Portland", "Oregon", 97201)
+      test_mailing_address.save()
+      expect(Mailing_Address.all()).to(eq([test_mailing_address]))
+    end
+  end
+
+  describe('.clear') do
+    it("empties out all of the mailing addresses")  do
+      Mailing_Address.new("123 Real Street", "Portland", "Oregon", 97201)
+      Mailing_Address.clear()
+      expect(Mailing_Address.all()).to(eq([]))
+    end
+  end
+
+
 
 end
